82c63e662c0127ae12794785e5519559f9376b8f
yousif <43048698+yousifhanani@users.noreply.github.com>
2021-04-10 19:43:13 -0400

    zombie shooter

---
+++ Final Game CSI 4380/Assets/Scripts/ZombieShooter/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Enemy : MonoBehaviour
+{
+	float Speed = 3f;
+
+	private GameObject wayPoint;
+	private Vector3 wayPointPos;
+
+	 
+	void Start()
+    {
+		wayPoint = GameObject.Find("wayPoint");
+		
+	
+	}
+	void Update()
+	{
+		
+		wayPointPos = new Vector3(wayPoint.transform.position.x, wayPoint.transform.position.y, wayPoint.transform.position.z);
+
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, wayPointPos - transform.position);
+		transform.position = Vector3.MoveTowards(transform.position, wayPointPos, Speed * Time.deltaTime);
+
+		if(transform.position == wayPointPos)
+        {
+			
+			SceneManager.LoadScene("GameOverZS");
+		}
+		
+
+	}
+	public void SetMoveSpeed(float speed)
+	{
+		Speed = speed;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Destroy(collision);
+		Destroy(gameObject);
+		
+
+
+
+	}
+
+}
---
+++ Final Game CSI 4380/Assets/Scripts/ZombieShooter/PlayerZS.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerZS : MonoBehaviour
+{
+    private float moveSpeed = 6;
+    [SerializeField] GameObject playerBullet;
+    [SerializeField] float projectileSpeed = 50f;
+
+    Coroutine firingCoroutine;
+
+    public GameObject wayPoint;
+    
+
+    private float timer = 0.5f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0)
+        {
+           
+            UpdatePosition();
+            timer = 0.5f;
+        }
+    }
+
+    void UpdatePosition()
+    {
+        
+        wayPoint.transform.position = transform.position;
+    }
+
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, -7.7f, 8.5f);
+
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, -4.3f, 3.8f);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+
+    private void Fire()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector2 direction = (Vector2)((worldMousePos - transform.position));
+            direction.Normalize();
+
+            GameObject laser = Instantiate(playerBullet, transform.position + (Vector3)(direction * 0.5f), Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+          
+        }
+
+    }
+
+}
